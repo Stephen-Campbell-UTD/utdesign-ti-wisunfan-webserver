9417a6d1eadf9534b44536b19922855e73f70ad3
AresDuong <91506129+AresDuong@users.noreply.github.com>
2021-11-16 17:23:54 -0600

    Update parsing.js

--- src/parsing.js
+++ src/parsing.js
@@ -1,7 +1,37 @@
+// uses data from propvalues
+// used in dbusCommands, index
+
+let propValues = require('./propValues.js')
+let numConnectedDevices = 0
+
 function parseConnectedDevices() {
   console.log('parsing connected devices');
+  devicesArray = propValues['connecteddevices'].split("\n")
+  connectedDevices = ""
+
+  for(let index in devicesArray) {
+    if (devicesArray[index] == "List of connected devices currently in routing table:" || devicesArray[index] == "") {
+        // Do nothing
+    } else if (devicesArray[index].substring(0, 28) == "Number of connected devices:") {
+        tempSplit = devicesArray[index].split(": ")
+        numConnectedDevices = tempSplit[1]
+    } else {
+        connectedDevices += devicesArray[index] + "|"
+    }
+  }
+
+  if(connectedDevices.length >= 1) {
+      connectedDevices = connectedDevices.slice(0, -1);
+  }
+
+  return connectedDevices
+}
+
+function getNumConnectedDevices() {
+  return numConnectedDevices
 }
 
 module.exports = {
-  parseConnectedDevices
+  parseConnectedDevices,
+  getNumConnectedDevices
 }
